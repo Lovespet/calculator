eb455bba2f2a777e510f8b32ee1cd84cc6460184
Olaide Popoola <80744497+Lovespet@users.noreply.github.com>
2022-05-22 19:41:08 +0000

    Modifying the add function
    Modifying the add function

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -12,6 +12,7 @@ exports.calculate = function(req, res) {
 
   var operations = {
    'add': function(a,b) { return a + b },
+    // Determine the new change
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
